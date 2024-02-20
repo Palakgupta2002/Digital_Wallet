ccebc80882f7dd8368692579a554b60b795678b8
Palak Gupta <120472704+Palakgupta2002@users.noreply.github.com>
2024-02-20 18:21:24 +0530

    Database is connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from "express"
-
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+dotenv.config()
 const app=express()
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("mongodb is connected")
+}).catch(err=>console.log("something wrong"))
 
 app.listen(3000,()=>{
     console.log("server is listen")
+})
+app.get('/',(req,res)=>{
+    res.json("hello")
 })

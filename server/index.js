29b39c87f9823c1e7ecebd10e5591979a5a78bd8
Palak Gupta <120472704+Palakgupta2002@users.noreply.github.com>
2024-02-20 19:38:33 +0530

    login,signUp api's

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import bodyParser from 'body-parser'
+import signUp from "./routes/signUp.js"
+import signIn from "./routes/signIn.js"
 dotenv.config()
-const app=express()
+const app=express();
+app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
@@ -12,6 +16,14 @@ mongoose.connect(process.env.MONGO_URL)
 app.listen(3000,()=>{
     console.log("server is listen")
 })
+
+//This is test api
 app.get('/',(req,res)=>{
     res.json("hello")
 })
+
+//This is for signUp api
+app.use('/auth',signUp)
+
+//This is signIn api
+app.use('/validateAuth',signIn)
